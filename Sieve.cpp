bf3f83fd6b6ea77ddd27b31a9b13edf074901ff2
Nishant Parmar <44092982+NISHANT026@users.noreply.github.com>
2021-07-30 19:35:22 +0530

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,17 +1,9 @@
-
-vector<bool> getPri(int n)
-{
-	vector<bool> primes(n+1,true);
-	primes[0]=false;
-	primes[1]=false;
-	for(int i=2;i<=n;i++)
-	{
-		if(primes[i])
-		{
-			for(int j=i*i;i<=n;j=j+p)
-			 primes[j]=false;
-		}
-	}
-	return primes;
-	
+void sieveOfErath(vector<bool> &sieve){
+    for (int i=2;i<sieve.size();i++){
+        if(!sieve[i])continue;
+        for (int j=i;j<sieve.size();j++){
+            if(i*j >=sieve.size())break;
+            sieve[i*j]=false;
+        }
+    }
 }
